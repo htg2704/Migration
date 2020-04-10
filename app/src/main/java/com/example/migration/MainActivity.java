338d43b16d9a87d631f5df3e708bc2f317726da3
Harshwardhan Tripathi <harshwardhan2704@gmail.com>
2020-04-10 15:41:25 +0530

    commit 2

--- app/src/main/java/com/example/migration/MainActivity.java
+++ app/src/main/java/com/example/migration/MainActivity.java
@@ -2,13 +2,65 @@ package com.example.migration;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    boolean doubleBackToExitPressedOnce = false;
+    Button enter_register,enter_data,enter_stats;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        enter_register = findViewById(R.id.btn_register);
+        enter_data = findViewById(R.id.btn_data);
+        enter_stats = findViewById(R.id.btn_stats);
+
+        enter_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),com.example.migration.Register.select_login_type.class);
+                startActivity(intent);
+            }
+        });
+        enter_data.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),com.example.migration.data.data_main.class);
+                startActivity(intent);
+            }
+        });
+        enter_stats.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),com.example.migration.Register.select_login_type.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (doubleBackToExitPressedOnce) {
+            super.onBackPressed();
+            return;
+        }
+
+        this.doubleBackToExitPressedOnce = true;
+        Toast.makeText(this, "Please click BACK again to exit", Toast.LENGTH_SHORT).show();
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                doubleBackToExitPressedOnce=false;
+            }
+        }, 2000);
     }
 }
---
+++ app/src/main/java/com/example/migration/data/data_main.java
@@ -0,0 +1,11 @@
+package com.example.migration.data;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+
+public class data_main extends AppCompatActivity {
+
+}

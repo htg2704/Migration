d8131b8206fb4f234424fee2658abe1f464ed4ea
Harshwardhan Tripathi <harshwardhan2704@gmail.com>
2020-04-10 17:18:28 +0530

    commit 2

--- app/src/main/java/com/example/migration/Register/select_login_type.java
+++ app/src/main/java/com/example/migration/Register/select_login_type.java
@@ -1,8 +1,17 @@
 package com.example.migration.Register;
 
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.migration.R;
 
 public class select_login_type extends AppCompatActivity {
 
-
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_login_type);
+    }
 }
--- app/src/main/java/com/example/migration/data/data_main.java
+++ app/src/main/java/com/example/migration/data/data_main.java
@@ -1,11 +1,18 @@
 package com.example.migration.data;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.PersistableBundle;
 
+import com.example.migration.R;
 
 
 public class data_main extends AppCompatActivity {
-
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_data_main);
+    }
 }

9c90346c5b94c6377162289082bf91b0a49512a1
DIVYESH_NABHOYA <divyeshnabhoya2@gmail.com>
2020-04-30 10:03:05 +0530

    UI changes-3

--- app/src/main/java/com/example/migration/Launcher.java
+++ app/src/main/java/com/example/migration/Launcher.java
@@ -2,15 +2,18 @@ package com.example.migration;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Notification;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
+import com.airbnb.lottie.LottieAnimationView;
+import com.airbnb.lottie.LottieDrawable;
+
 public class Launcher extends AppCompatActivity {
 
-        Notification notification;
+
+    LottieAnimationView lottieAnimationView;
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
@@ -23,6 +26,7 @@ public class Launcher extends AppCompatActivity {
                         Launcher.this.finish();
                     }
                 },2000);
+
 
         }
     }

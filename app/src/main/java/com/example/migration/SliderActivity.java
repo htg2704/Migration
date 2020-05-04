2af1f77a8c278c9e369262ca2ccfda2030a7681f
DIVYESH_NABHOYA <divyeshnabhoya2@gmail.com>
2020-05-04 20:02:51 +0530

    update

--- app/src/main/java/com/example/migration/SliderActivity.java
+++ app/src/main/java/com/example/migration/SliderActivity.java
@@ -1,12 +1,19 @@
 package com.example.migration;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.smarteist.autoimageslider.DefaultSliderView;
@@ -18,54 +25,58 @@ public class SliderActivity extends AppCompatActivity {
 
     private long backPressedTime = 0;
     SliderLayout sliderLayout;
-    ImageButton myImageButton;
+    LinearLayout welcome;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_slider);
-        myImageButton = (ImageButton) findViewById(R.id.wel);
+
+            welcome = (LinearLayout) findViewById(R.id.wel);
 
 
-        myImageButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent beach = new  Intent(SliderActivity.this, MainActivity.class);
-                startActivity(beach);
-            }
-        });
+            welcome.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent beach = new  Intent(SliderActivity.this, MainActivity.class);
+                    startActivity(beach);
+                }
+            });
 
 
 
-        sliderLayout = findViewById(R.id.imageSlider);
-        sliderLayout.setIndicatorAnimation(IndicatorAnimations.FILL); //set indicator animation by using SliderLayout.Animations. :WORM or THIN_WORM or COLOR or DROP or FILL or NONE or SCALE or SCALE_DOWN or SLIDE and SWAP!!
-        sliderLayout.setScrollTimeInSec(1); //set scroll delay in seconds :
+            sliderLayout = findViewById(R.id.imageSlider);
+            sliderLayout.setIndicatorAnimation(IndicatorAnimations.FILL);
+            sliderLayout.setScrollTimeInSec(2);
 
-        setSliderViews();
+            setSliderViews();
 
-    }
-
-    private void setSliderViews() {
-
-        for (int i = 0; i <= 2; i++) {
-
-            DefaultSliderView sliderView = new DefaultSliderView(this);
-
-
-            switch (i) {
-                case 0:
-                    sliderView.setImageUrl("https://i.postimg.cc/3RmgXv3L/s1.png");
-                    break;
-                case 1:
-                    sliderView.setImageUrl("https://i.postimg.cc/9QNNX0Cx/s2.png");
-                    break;
-                case 2:
-                    sliderView.setImageUrl("https://i.postimg.cc/DZfnX5Sv/s3.png");
-                    break;
-
-            }
-            sliderLayout.addSliderView(sliderView);
         }
-    }
+
+        private void setSliderViews() {
+
+            for (int i = 0; i <= 2; i++) {
+
+                DefaultSliderView sliderView = new DefaultSliderView(this);
+
+
+                switch (i) {
+                    case 0:
+                        sliderView.setImageUrl("https://i.postimg.cc/3RmgXv3L/s1.png");
+                        break;
+                    case 1:
+                        sliderView.setImageUrl("https://i.postimg.cc/9QNNX0Cx/s2.png");
+                        break;
+                    case 2:
+                        sliderView.setImageUrl("https://i.postimg.cc/DZfnX5Sv/s3.png");
+                        break;
+
+
+                }
+                sliderLayout.addSliderView(sliderView);
+            }
+
+
+        }
 
 
     @Override
@@ -82,3 +93,4 @@ public class SliderActivity extends AppCompatActivity {
     }
 
 }
+
